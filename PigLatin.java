4f6ec6012fef41d3ab3f019274b8bdf4a83ccdd7
TheEpikDuck <zhujul24@7hills.org>
2023-09-08 12:56:51 -0400

    stuff

---
+++ PigLatin.java
@@ -0,0 +1,18 @@
+//
+import java.util.Scanner;
+
+public class PigLatin 
+{
+    public static void (String[] args)
+    //prompt user for word
+    String word = "";
+    len = word.length();
+    for (int i = 0; i< len; i++);
+    {
+        if (word[i].equals("a"))
+    
+    {
+        phrase.substring(i, i+1); //up to but not including i+1
+    }
+    phrase[i]
+}
