8f6f3aae0685e9cd00b6a9dee01efa04fa29bea1
TheEpikDuck <zhujul24@7hills.org>
2023-09-08 13:10:21 -0400

    stuff

--- PigLatin.java
+++ PigLatin.java
@@ -6,13 +6,18 @@ public class PigLatin
     public static void (String[] args)
     //prompt user for word
     String word = "";
+    word = word.toLowerCase();
     len = word.length();
     for (int i = 0; i< len; i++);
     {
         if (word[i].equals("a"))
-    
-    {
-        phrase.substring(i, i+1); //up to but not including i+1
+        {
+            
+            System.out.println("Found the letter a");
+        }
+
+        word.substring(i, i+1); //up to but not including i+1
+        
     }
     phrase[i]
 }
