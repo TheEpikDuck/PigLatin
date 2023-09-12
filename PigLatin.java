a6881e09d081f82b7ac80fa6f7f843886b983a62
TheEpikDuck <zhujul24@7hills.org>
2023-09-11 23:28:55 -0400

    i got the basic code down have to fix some things

--- PigLatin.java
+++ PigLatin.java
@@ -4,20 +4,39 @@ import java.util.Scanner;
 public class PigLatin 
 {
     public static void (String[] args)
-    //prompt user for word
-    String word = "";
-    word = word.toLowerCase();
-    len = word.length();
-    for (int i = 0; i< len; i++);
-    {
-        if (word[i].equals("a"))
-        {
-            
-            System.out.println("Found the letter a");
-        }
+    
+    //Declare string for userSentence and pigLatin
+    String userSentence; 
 
-        word.substring(i, i+1); //up to but not including i+1
-        
+    //Assign vowels to char
+    char a = 'a';
+    char e = 'e';
+    char i = 'i';
+    char o = 'o';
+    char u = 'u';
+
+    //Ask user for input and save to userSentence variable
+    System.out.println("Enter a sentence: ");
+    userSentence = input.nextLine();
+
+    //Split the userSentence into an array
+    String[] userWords = userSentence.split(" ");
+
+    //This will go through the words and locate vowels
+    for(int k = 0; k < userWords.length; k++) {
+
+        //Change letters in userWords to lower case
+        String temp = userWords[k].toLowerCase();
+        char c = temp.charAt(0); 
+
+            //If first character is equal to a vowel
+            if(c == a || c == e || c == i || c == o || c == u) {
+                System.out.println(temp + "\t" + temp + "way");
+                }
+
+            else {
+                //print the ones that start with a consonant
+                System.out.println(temp + "\t" + temp + "ay");
+            }
     }
-    phrase[i]
 }
